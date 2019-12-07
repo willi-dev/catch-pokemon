36dde1e127c6f607b0ff22705e4bbff29527393a
willi-dev <willi.ilmukomputer@gmail.com>
2019-12-07 23:56:45 +0700

    [pokemon-list] add PokemonImage component

---
+++ src/components/pokemon-item.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const PokemonItem = () => (
+  <div className="pokemon-item">
+    pokemon item
+  </div>
+)
+
+export default PokemonItem
--- src/components/pokemon-list-item.js
+++ src/components/pokemon-list-item.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
+import PokemonImage from './pokemon-image'
+
 const PokemonListItem = props => (
-  <div>
-    <img src={`https://img.pokemondb.net/artwork/${props.name}.jpg`} alt={props.name}/>
-    
+  <div className="pokemon-list-item">
+    <PokemonImage src={`https://img.pokemondb.net/artwork/${props.name}.jpg`} alt={props.name}/>
     <Link to={`/pokemon/${props.name}`}>{props.name}</Link>
   </div>
 )
