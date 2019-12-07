dc69a24f860f470687ec69a3813b2d329ab2d12e
willi-dev <willi.ilmukomputer@gmail.com>
2019-12-07 23:35:51 +0700

    [pokemon-list] add link

--- src/components/pokemon-list-item.js
+++ src/components/pokemon-list-item.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const PokemonListItem = props => (
   <div>
     <img src={`https://img.pokemondb.net/artwork/${props.name}.jpg`} alt={props.name}/>
-    {props.name}
+    
+    <Link to={`/pokemon/${props.name}`}>{props.name}</Link>
   </div>
 )
 
