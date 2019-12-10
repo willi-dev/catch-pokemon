19c30c9846ae3fc325a785a5a7ae3be3807da0c9
willi-dev <willi.ilmukomputer@gmail.com>
2019-12-10 20:54:58 +0700

    [pokemon-list] progress fetching detail

--- src/pages/pokemon-details.js
+++ src/pages/pokemon-details.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { connect } from 'react-redux'
+// import shortid from 'shortid'
 
-const PokemonDetails = () => {
+import { fetchingPokemonDetail } from '../store/pokemon-details/action'
+
+const PokemonDetails = props => {
+  const {
+    name,
+    isLoading,
+    // detailPokemon,
+    fetchingPokemonDetail
+  } = props
+
+  useEffect(() => {
+    fetchingPokemonDetail({name})
+  }, [])
+
   return (
     <>
+      {
+        isLoading && (
+          <p>Loading...</p>
+        )
+      }
       <p>Pokemon Details</p>
     </>
   )
 }
 
-export default PokemonDetails
+const mapStateToProps = (state, ownProps) => ({
+  name: ownProps.match.params.pokemon,
+  isLoading: state.detailPokemon.isLoading,
+  detailPokemon: state.detailPokemon.detail 
+})
+
+const mapDispatchToProps = dispatch => ({
+  fetchingPokemonDetail: pokemon => {
+    dispatch(fetchingPokemonDetail(pokemon))
+  }
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(PokemonDetails)
